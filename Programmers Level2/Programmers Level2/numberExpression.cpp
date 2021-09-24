e82e9f4cf092edc79ff57a155a8480d5da49a030
Harriet Lee <lharriet27@gmail.com>
2021-09-25 00:20:16 +0900

    [programmers] Level2 numberExpression

---
+++ Programmers Level2/Programmers Level2/numberExpression.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+숫자의 표현
+
+Finn은 요즘 수학공부에 빠져 있습니다. 
+수학 공부를 하던 Finn은 자연수 n을 연속한 자연수들로 표현 하는 방법이 여러개라는 사실을 알게 되었습니다. 
+예를들어 15는 다음과 같이 4가지로 표현 할 수 있습니다.
+
+1 + 2 + 3 + 4 + 5 = 15
+4 + 5 + 6 = 15
+7 + 8 = 15
+15 = 15
+자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
+
+*/
+
+int solution(int n) {
+	int answer = 0;
+	int startNum = 1, nextNum = 2, totalNum = startNum;
+	while (startNum <= n)
+	{
+		if (startNum == n)
+		{
+			answer++;
+			break;
+		}
+
+		totalNum += nextNum;
+
+		if (totalNum < n)
+		{
+			nextNum++;
+		}
+		else if (totalNum > n)
+		{
+			startNum++;
+			totalNum = startNum;
+			nextNum = startNum + 1;
+		}
+		else if (totalNum == n)
+		{
+			startNum++;
+			answer++;
+		}
+
+	}
+	return answer;
+}
+
+int main()
+{
+	cout << solution(15);
+	return 0;
+}
